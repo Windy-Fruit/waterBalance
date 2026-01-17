03bacd6be2f866fd0425a9c52a6aa77ffbc4fcf9
Лидия <lida_demina06@mail.ru>
2026-01-17 23:16:36 +0500

    Add models for user profile, water norm and intake

--- water/models.py
+++ water/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    height = models.IntegerField()
+    activity_level = models.CharField(max_length=50)
+    climate = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+    
+class DailyWaterNorm(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    calculated_norm = models.FloatField()
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Water norm for {self.user.username}"
+    
+class WaterIntake(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField(help_text="Amount of water in ml")
+    intake_time = models.DateTimeField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.amount} ml - {self.user.username}"
