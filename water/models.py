a6d4242ae2036e000fb17d13af20211325f34191
Лидия <lida_demina06@mail.ru>
2026-01-19 17:18:14 +0500

    Automatic calculation of water standards

--- water/models.py
+++ water/models.py
@@ -6,20 +6,47 @@ class UserProfile(models.Model):
     age = models.IntegerField()
     weight = models.FloatField()
     height = models.IntegerField()
-    activity_level = models.CharField(max_length=50)
-    climate = models.CharField(max_length=50)
+    activity_level = models.CharField(max_length=20)
+    climate = models.CharField(max_length=20)
 
     def __str__(self):
         return f"Profile of {self.user.username}"
     
 class DailyWaterNorm(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    calculated_norm = models.FloatField()
+    calculated_norm = models.FloatField(blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Water norm for {self.user.username}"
+    def calculate_norm(self):
+        profile = UserProfile.objects.get(user=self.user)
+
+        base = profile.weight * 30
+
+        activity_map = {
+            'low': 1.0,
+            'medium': 1.2,
+            'high': 1.4,
+        }
+
+        climate_map = {
+            'cold': 1.0,
+            'normal': 1.1,
+            'hot': 1.2,
+        }
+
+        activity_coef = activity_map.get(profile.activity_level, 1)
+        climate_coef = climate_map.get(profile.climate, 1)
+
+        return base * activity_coef * climate_coef
     
+    def save(self, *args, **kwargs):
+        if not self.calculated_norm:
+            self.calculated_norm = self.calculate_norm()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.calculated_norm:.0f} ml"
+        
 class WaterIntake(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.IntegerField(help_text="Amount of water in ml")
