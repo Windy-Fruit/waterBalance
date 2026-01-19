85b97bcbb44bc09ee8c1ab7900d84e46a6d12206
Лидия <lida_demina06@mail.ru>
2026-01-19 14:09:22 +0500

    Configure admin for water models

--- water/admin.py
+++ water/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile, DailyWaterNorm, WaterIntake
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "age", "weight", "height", "activity_level", "climate")
+    search_fields = ("user__username",)
+
+
+@admin.register(DailyWaterNorm)
+class DailyWaterNormAdmin(admin.ModelAdmin):
+    list_display = ("user", "calculated_norm", "date_created")
+    list_filter = ("date_created",)
+
+
+@admin.register(WaterIntake)
+class WaterIntakeAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "intake_time", "date")
+    list_filter = ("date",)
+    search_fields = ("user__username",)
